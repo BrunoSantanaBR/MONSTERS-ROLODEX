b386a1e93acd9992b136652a35ae236a89a56644
BrunoSantanaBR <bruno.raphael.marques.santana@gmail.com>
2020-03-01 14:10:28 +0100

    Updates

---
+++ precache-manifest.c88fe8b06ba9b738bbe6a77825d2492a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4632279d74176c08aa69519b4926a6b8",
+    "url": "/MONSTERS-ROLODEX/index.html"
+  },
+  {
+    "revision": "8b4eebccd8d084b0d059",
+    "url": "/MONSTERS-ROLODEX/static/css/main.4fdecf2a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/MONSTERS-ROLODEX/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MONSTERS-ROLODEX/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4eebccd8d084b0d059",
+    "url": "/MONSTERS-ROLODEX/static/js/main.d9a86227.chunk.js"
+  },
+  {
+    "revision": "441e32ad446c87de35ff",
+    "url": "/MONSTERS-ROLODEX/static/js/runtime-main.62d4bca8.js"
+  }
+]);
